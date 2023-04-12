8d1b602c311b5464d0d39c5f43a0ffe6c74affe8
Fred Morris <FredrickCPMorris@hotmail.com>
2023-04-12 13:27:29 -0500

    Updated URL_HOST variable.

--- src/urlHost.js
+++ src/urlHost.js
@@ -1,4 +1,6 @@
 export const URL_HOST =
   process.env.NODE_ENV === "production"
-    ? "https://cors-everywhere-me.herokuapp.com/http://ytcbackend-env.eba-nm9x2qaq.us-east-1.elasticbeanstalk.com"
+    ? "http://ytcbackend-env.eba-nm9x2qaq.us-east-1.elasticbeanstalk.com"
     : "http://127.0.0.1:8000";
+
+// https://cors-everywhere-me.herokuapp.com
